a2d30b080bbe6a9adefc2b825cb1ad55e3ea5355
SOunit <workbookwork7@gmail.com>
2021-12-07 18:05:01 -0800

    add samples

---
+++ 03_big_o/38_compressBoxesTwice.js
@@ -0,0 +1,9 @@
+function compressBoxesTwice(boxes, boxes2) {
+  boxes.forEach((element) => {
+    console.log(element);
+  });
+
+  boxes2.forEach((element) => {
+    console.log(element);
+  });
+}
