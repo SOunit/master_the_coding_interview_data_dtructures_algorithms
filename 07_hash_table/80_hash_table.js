f02a8e4fcb57c2f5e69d53410cf7e0e703aaf129
SOunit <workbookwork7@gmail.com>
2021-12-10 08:30:29 -0800

    add sample

---
+++ 07_hash_table/80_hash_table.js
@@ -0,0 +1,29 @@
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  }
+
+  set(key, value) {
+    const index = this._hash(key);
+    this.data[index] = value;
+  }
+
+  get(key) {
+    const index = this._hash(key);
+    return this.data[index];
+  }
+}
+
+const myHashTable = new HashTable(50);
+myHashTable.set("grapes", 10000);
+myHashTable.get("grapes");
+myHashTable.set("apples", 9);
+myHashTable.get("apples");
