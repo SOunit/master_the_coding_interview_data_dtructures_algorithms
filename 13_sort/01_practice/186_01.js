aed53fde8914917ea851103bdc350c7a8db5f206
SOunit <workbookwork7@gmail.com>
2021-12-14 17:42:01 -0800

    add sample

--- 13_sort/01_practice/186_01.js
+++ 13_sort/01_practice/186_01.js
@@ -1,15 +1,34 @@
+// 1. babble sort       : only for education
+// 2. selection sort    : only for education
+// 3. insert sort       : if items are small number of collection
+// 4. merge sort        : good
+// 5. quick sort        : good but worst case
+
 //#1 - Sort 10 schools around your house by distance:
+// insert sort, for small number of array
 
 //#2 - eBay sorts listings by the current Bid amount:
+// merge sort?
+// quick sort?
 
 //#3 - Sport scores on ESPN
+// merge sort?
+// quick sort?
 
 //#4 - Massive database (can't fit all into memory) needs to sort through past year's user data
+// quick sort, less space complexity
 
 //#5 - Almost sorted Udemy review data needs to update and add 2 new reviews
+// insertion sort
 
 //#6 - Temperature Records for the past 50 years in Canada
+// merge sort
+// quick sort
 
 //#7 - Large user name database needs to be sorted. Data is very random.
+// quick sort
+// merge sort
 
 //#8 - You want to teach sorting for the first time
+// babble sort
+// selection sort
